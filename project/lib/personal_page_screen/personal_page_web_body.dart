81f13e2b9bb541648c190ea513ba8de81087daf9
Martim Gamboa <m.gamboa@campus.fct.unl.pt>
2023-06-22 17:42:29 +0100

    Logout v1.0

--- project/lib/personal_page_screen/personal_page_web_body.dart
+++ project/lib/personal_page_screen/personal_page_web_body.dart
@@ -5,6 +5,8 @@ import 'package:UniVerse/find_screen/findTest/right_side.dart';
 import 'package:UniVerse/main_screen/components/about_bottom.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:UniVerse/consts/api_consts.dart';
 
 import '../components/web_menu_card.dart';
 import '../main_screen/components/about_bottom_body.dart';
@@ -58,7 +60,18 @@ class PersonalWebBodyState extends State<PersonalWebBody> {
                       children:  [
                         Column(
                           children: <Widget>[
-                            WebMenuCard(text: 'Logout', description: 'Vê e altera as tuas informações pessoais.', icon: Icons.logout_outlined),
+                            WebMenuCard(
+                              text: 'Logout',
+                              description: 'Vê e altera as tuas informações pessoais.',
+                              icon: Icons.logout_outlined,
+                              press: () async {
+                                final response = await http.post(
+                                  Uri.parse(baseUrl + logoutUrl),
+                                );
+                                print(response.statusCode);
+                                return response.statusCode;
+                              },
+                            ),
                             WebMenuCard(text: 'O Meu Perfil', description: 'Vê e altera as tuas informações pessoais.', icon: Icons.person_outline),
                             WebMenuCard(text: 'QR Scan', description: 'Entra numa sala digitalizando o código QR na sua porta.', icon: Icons.qr_code_scanner_outlined),
                             WebMenuCard(text: 'Comprovativo', description: 'Acede ao comprovativo da tua vinculação com a FCT NOVA.',icon: Icons.card_membership_outlined),
