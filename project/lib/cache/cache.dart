c9cdbf108320e1ac4088c0f482233063533f4405
Martim Gamboa <m.gamboa@campus.fct.unl.pt>
2023-07-07 14:17:00 +0100

    Cache implementation

---
+++ project/lib/cache/cache.dart
@@ -0,0 +1,28 @@
+import 'package:hive/hive.dart';
+
+class Cache {
+  static final _instance = Cache._internal();
+
+  Cache._internal();
+
+  factory Cache.init() {
+    return _instance;
+  }
+
+  factory Cache() {
+    return _instance;
+  }
+
+  String read(String url) {
+    Box cache = Hive.box('cache');
+
+    return cache.get(url) ?? '';
+  }
+
+  write(String url, String data) {
+    Box cache = Hive.box('cache');
+
+    cache.put(url, data);
+  }
+
+}
