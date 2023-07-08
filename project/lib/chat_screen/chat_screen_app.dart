e8ab19286ede07d323ca1b318bbc94d7d68723fe
Martim Gamboa <m.gamboa@campus.fct.unl.pt>
2023-07-08 02:10:20 +0100

    Order chat by posted time

--- project/lib/chat_screen/chat_screen_app.dart
+++ project/lib/chat_screen/chat_screen_app.dart
@@ -51,6 +51,7 @@ class _MyChatPageState extends State<ChatPageApp> {
       _chatStream = FirebaseDatabase.instance
           .ref()
           .child('forums/${widget.forumID}/feed/')
+          .orderByChild("posted")
           .onValue
           .listen((event) {
         var snapshot = event.snapshot;
