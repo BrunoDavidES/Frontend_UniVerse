5956d9823648ef8c23ff9235e527bb9f0e51b751
Martim Gamboa <m.gamboa@campus.fct.unl.pt>
2023-07-09 00:52:10 +0100

    js sendEmail

--- project/functions/index.js
+++ project/functions/index.js
@@ -1,19 +1,34 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendEmail = functions.https.onCall(async (data, context) => {
+  const { recipient, subject, body } = data;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "capi.crew@gmail.com", // Replace with your Gmail email address
+      pass: "pe2doManelExplodiu", // Replace with your Gmail password or an app-specific password
+    },
+  });
+
+  const mailOptions = {
+    from: "capi.crew@gmail.com", // Replace with your Gmail email address
+    to: recipient,
+    subject: subject,
+    text: body,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+    return { success: true };
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new functions.https.HttpsError("internal", "Email sending failed");
+  }
+});
+
